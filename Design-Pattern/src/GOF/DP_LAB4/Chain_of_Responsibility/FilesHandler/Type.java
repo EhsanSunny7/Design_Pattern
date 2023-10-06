cf7b94dc2032857280907c8718b63a1ddcf6dfd1
EHSANSUNNY <www.ehsansunny555@gmail.com>
2023-10-06 10:01:50 +0500

    Task Completed of  Chain of Responsibilities
    Task Completed of  Chain of Responsibilities

---
+++ Design-Pattern/src/GOF/DP_LAB4/Chain_of_Responsibility/FilesHandler/Type.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GOF.DP_LAB4.Chain_of_Responsibility.FilesHandler;
+
+/**
+ *
+ * @author FA20-BSE-068
+ */
+public enum Type {
+	TXT, DOC, PDF, MP3, GIF
+}
