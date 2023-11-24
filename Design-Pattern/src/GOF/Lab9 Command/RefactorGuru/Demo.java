c1badfedf0325cfc5626674bd8c5584f368d33eb
EHSANSUNNY <www.ehsansunny555@gmail.com>
2023-11-24 09:56:31 +0500

    Update Demo.java

--- Design-Pattern/src/GOF/Lab9 Command/RefactorGuru/Demo.java
+++ Design-Pattern/src/GOF/Lab9 Command/RefactorGuru/Demo.java
@@ -7,7 +7,7 @@ package GOF.BehavioralPattern.Command.RefactorGuru;
 
 /**
  *
- * @author FA20-BSE-042
+ * @author FA20-BSE-068
  */
 
 import GOF.BehavioralPattern.Command.RefactorGuru.editor.Editor;
