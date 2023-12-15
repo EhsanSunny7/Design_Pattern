7996ade4f0158fffe239dd65f3814e8e5adc28d4
EHSANSUNNY <www.ehsansunny555@gmail.com>
2023-12-15 11:05:28 +0500

    Update AndExpression.java

--- Design-Pattern/src/GOF/Interpreter Pattern/TutorialPoint-Exp/AndExpression.java
+++ Design-Pattern/src/GOF/Interpreter Pattern/TutorialPoint-Exp/AndExpression.java
@@ -7,7 +7,7 @@ package GOF.BehavioralPattern.Interpreter.TutorialPointExample;
 
 /**
  *
- * @author FA20-BSE-042
+ * @author FA20-BSE-068
  */
 public class AndExpression implements Expression {
 	 
