4953aff671ea2f57a6ff57de1feab4ca33986c8c
EHSANSUNNY <www.ehsansunny555@gmail.com>
2023-12-22 10:46:34 +0500

    Update Shape.java

--- Design-Pattern/src/GOF/Memento Lab/refactoringguru/shapes/Shape.java
+++ Design-Pattern/src/GOF/Memento Lab/refactoringguru/shapes/Shape.java
@@ -3,11 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package GOF.BehavioralPattern.Memento.refactoringguru.shapes;
+
 
 /**
  *
- * @author FA20-BSE-042
+ * @author FA20-BSE-068
  */
 import java.awt.*;
 import java.io.Serializable;
