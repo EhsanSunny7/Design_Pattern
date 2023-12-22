55ac6fa21dd118dd87c338f4cf522a81931d4587
EHSANSUNNY <www.ehsansunny555@gmail.com>
2023-12-22 10:18:17 +0500

    Update AdvancedRemote.java

--- Design-Pattern/src/GOF/Bridge Lab/Refactoring GuruExample/AdvancedRemote.java
+++ Design-Pattern/src/GOF/Bridge Lab/Refactoring GuruExample/AdvancedRemote.java
@@ -7,7 +7,7 @@ package GOF.StructuralPattern.Bridge.RFExample.remotes;
 
 /**
  *
- * @author FA20-BSE-042
+ * @author FA20-BSE-068
  */
 import GOF.StructuralPattern.Bridge.RFExample.devices.Device;
 
